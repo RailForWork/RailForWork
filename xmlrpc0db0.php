fd7627d90f2a23a8a7dea2cd091cf43681667d6f
lolo <akarinserver@gmail.com>
2021-09-05 20:20:49 +0800

    change domain
    change all rail.moe to girls.rail.moe

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://rail.moe</homePageLink>
+		<homePageLink>https://girls.rail.moe</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://rail.moe/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://rail.moe/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://rail.moe/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://rail.moe/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://rail.moe/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://girls.rail.moe/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://girls.rail.moe/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://girls.rail.moe/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://girls.rail.moe/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://girls.rail.moe/wp-json/" />
 			</apis>
 	</service>
 </rsd>
